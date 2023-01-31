5079c19b24ad752e049b258ea01c13982f4df8a3
zaharius63 <zaharius63@gmail.com>
2023-01-31 12:56:20 +0300

    broascasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,12 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+//                'cluster' => env('PUSHER_APP_CLUSTER'),
                 // 'useTLS' => true,
-                'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'https'
+//                'encrypted' => true,
+//                'host' => '127.0.0.1',
+//                'port' => 6001,
+//                'scheme' => 'https'
 //                'cluster' => env('PUSHER_APP_CLUSTER'),
 //                'useTLS' => true,
 //                'encrypted' => false,
